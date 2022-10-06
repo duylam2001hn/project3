41af04a8ffc795e32a5ea982b9240f567a2f7db6
ghost18 <112473909+duylam2001hn@users.noreply.github.com>
2022-10-06 11:49:53 +0700

    Update artist.cs
    add some field

--- Gallery art 3/Models/artist.cs
+++ Gallery art 3/Models/artist.cs
@@ -30,6 +30,18 @@ namespace Gallery_art_3.Models
         public string Expire_date { get; set; }
 
         public int Cus_id { get; set; }
+        
+        [Required]
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Country { get; set; }
 
         public virtual customer customer { get; set; }
 
