9428a8670b583d169f9cde82548c0d671bd2ab0c
ghost18 <112473909+duylam2001hn@users.noreply.github.com>
2022-10-06 11:15:44 +0700

    Update customer.cs
    add status for customer

--- Gallery art 3/Models/customer.cs
+++ Gallery art 3/Models/customer.cs
@@ -25,26 +25,22 @@ namespace Gallery_art_3.Models
         [StringLength(100)]
         public string FullName { get; set; }
 
-        [Required]
-        [StringLength(100)]
         
-        public string Address { get; set; }
 
         [Required]
         [StringLength(100)]
         [EmailAddress]
         [RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-??]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
-
+        
+        public int Status { get; set; }
         
         [StringLength(100)]
         [DataType(DataType.Password)]
         
         public string Password { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        public string City { get; set; }
+        
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<artist> artists { get; set; }
